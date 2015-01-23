c7a9a8c07a2bdc4edd05c644e2a6c79a3cfb84b1
TomPickett <Pickett.tom7@gmail.com>
2015-01-22 20:30:37 -0500

    Created basic Hero Class

---
+++ src/Hero.java
@@ -0,0 +1,43 @@
+public class Hero {
+	//Private Property************************
+	private int strength;
+	private int speed;
+	private int health;
+	
+	//Public Properties***********************
+	public String name;
+	
+	//Getters*********************************
+	public int getStrength() {
+		return strength;
+	}
+	
+	public int getSpeed() {
+		return speed;
+	}
+
+	public int getHealth() {
+		return health;
+	}
+
+	//Constructor*****************************
+	public Hero(String name) {
+		this.name = name;
+		generateAbilities();
+	}
+	
+	//Private Methods*************************
+	private void generateAbilities() {
+		
+	}
+	
+	public void fight() {
+		
+	}
+	
+	public void run() {
+		
+	}
+
+	
+}
